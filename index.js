59f11381c1c60662568a3e151b349bfc1f175daf
Shelly <Emmpa@fake.com>
2017-10-20 13:19:37 -0700

    v1.4.0:  - specify both app name and version  - add serviceContext to remote logging  - add logName, resource to remote stream if authenticating remotely

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bunyan = require('bunyan');
 module.exports = (options) => {
     if (!options) options = {};
 
-    if (typeof options.app_name === 'undefined') options.app_name = 'app';
+    if (typeof options.app === 'undefined') options.app = {name: options.app_name};
+    if (typeof options.app.name === 'undefined') options.app.name = 'app';
     if (typeof options.local_level === 'undefined') options.local_level = process.env.LOG_LEVEL_LOCAL;
     if (typeof options.remote_level === 'undefined') options.remote_level = process.env.LOG_LEVEL_REMOTE;
 
@@ -13,25 +14,31 @@ module.exports = (options) => {
 };
 
 class Logger {
-    constructor(appName, localLevel, remoteLevel, remoteAuth) {
+    constructor(appParams, localLevel, remoteLevel, remoteAuth) {
         this.streams = [];
 
+        let options = {};
         if (localLevel) {
             this.addLocalStream(localLevel);
         }
 
         if (remoteLevel) {
-            this.addRemoteStream(remoteLevel, remoteAuth);
+            this.addRemoteStream(remoteLevel, remoteAuth, appParams);
+            Object.assign(options, {
+                serviceContext: {
+                    service: appParams.name,
+                    version: appParams.version,
+                }});
         }
 
-        this.logger = bunyan.createLogger({
-            name: appName,
-            streams: this.streams,
+        Object.assign(options, {
+            name: appParams.name,
+            streams: this.streams
         });
+        this.logger = bunyan.createLogger(options);
 
-        // TODO: add module support
         this.logger.module = (moduleName) => {
-            return this.logger;
+            return bunyan.createLogger(Object.assign({}, options, {module: moduleName}));
         };
     }
 
@@ -46,7 +53,16 @@ class Logger {
         });
     }
 
-    addRemoteStream(level, auth) {
+    addRemoteStream(level, auth, appParams) {
+        if (auth) {
+            auth.logName = auth.logName || appParams.name;
+            auth.resource = auth.resource || {
+                type: 'project',
+                labels: {
+                    project_id: appParams.name,
+                },
+            };
+        }
         const loggingBunyan = require('@google-cloud/logging-bunyan')(auth);
         let stream = loggingBunyan.stream(level);
         this.streams.push(stream);
